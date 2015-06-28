ba06af71096eb4b499aa174c3dce67e5d5c34160
Soren Peterson <soren468@gmail.com>
2015-06-28 13:07:28 -0500

    mv createjs into client folder in an attempt to not use it on the server

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: 'sorenpeterson:createjs',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'CreateJS packaged for meteor. Current versions: EaselJS 0.8.1, TweenJS 0.6.1, SoundJS 0.6.1, PreloadJS 0.6.1',
+  summary: 'EaselJS 0.8.1, TweenJS 0.6.1, SoundJS 0.6.1, PreloadJS 0.6.1',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/sorenpeterson/createjs-meteor.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('create-js.2015.05.21.combined.js');
+  api.addFiles('client/create-js.2015.05.21.combined.js');
 });
 
 Package.onTest(function(api) {
