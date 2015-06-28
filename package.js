7a16e6f0a00e0f70898089a55f71566af102a9e4
Soren Peterson <soren468@gmail.com>
2015-06-28 12:49:40 -0500

    bring in content from cdn and upate package metadate|

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'createjs',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'CreateJS packaged for meteor. Current versions: EaselJS 0.8.1, TweenJS 0.6.1, SoundJS 0.6.1, PreloadJS 0.6.1',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/sorenpeterson/createjs-meteor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('createjs.js');
+  api.addFiles('create-js.2015.05.21.combined.js');
 });
 
 Package.onTest(function(api) {
