52eb030a0e730b9cb87995c2e3b7651f646a787b
Soren Peterson <soren468@gmail.com>
2015-06-28 13:15:39 -0500

    changed packagejs to only use on client side

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
 	name: 'sorenpeterson:createjs',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'EaselJS 0.8.1, TweenJS 0.6.1, SoundJS 0.6.1, PreloadJS 0.6.1',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/sorenpeterson/createjs-meteor.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	version: '0.0.2',
+	// Brief, one-line summary of the package.
+	summary: 'CreateJS packaged for meteor. Current versions: EaselJS 0.8.1, TweenJS 0.6.1, SoundJS 0.6.1, PreloadJS 0.6.1',
+		// URL to the Git repository containing the source code for this package.
+	git: 'https://github.com/sorenpeterson/createjs-meteor.git',
+		// By default, Meteor will default to using README.md for documentation.
+		// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('client/create-js.2015.05.21.combined.js');
+	api.versionsFrom('1.1.0.2');
+	api.addFiles('create-js.2015.05.21.combined.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('sorenpeterson:createjs');
-  api.addFiles('createjs-tests.js');
+	api.use('tinytest');
+	api.use('createjs');
+	api.addFiles('createjs-tests.js');
 });
